77dd787dc13a546834ce793a7d7632ff97155895
zeeshanlatif07 <m.zeeshanlatif07@gmail.com>
2024-03-16 08:25:57 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+document.getElementById("my-button").onclick=function(){
+    document.getElementById("st").innerHTML='<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">M.Zeeshan Latif</div>'
+    document.getElementById("my-button").onclick=alert("M.Zeeshan Latif")
+    document.getElementById("st").style.color=""
+}
+document.getElementById("my-button1").onclick=function(){
+    document.getElementById("st").innerHTML='<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">123456789</div>'
+    document.getElementById("st1").innerHTML='<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">123456789</div>'
+    document.getElementById("my-button1").onclick=alert(123456789)
+}
+document.getElementById("my-button2").onclick=function(){
+    document.getElementById("st").innerHTML='<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">Variable</div>'
+    document.getElementById("st1").innerHTML='<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">Variable</div>'
+}
+document.getElementById("my-button3").onclick=function(){
+    document.getElementById("st").innerHTML='<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">zeeshanLatif</div>'
+    document.getElementById("st1").innerHTML='<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">zeeshanLatif</div>'
+}
+document.getElementById("my-button4").onclick=function(){
+    document.getElementById("st").innerHTML='<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">5+5</div>'
+    document.getElementById("st1").innerHTML='<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">=10</div>'
+}
+document.getElementById("my-button5").onclick=function(){
+    document.getElementById("st").innerHTML='<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">20-10</div>'
+    document.getElementById("st1").innerHTML='<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">=10</div>'
+}
+document.getElementById("my-button6").onclick=function(){
+    document.getElementById("st").innerHTML='<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">5*5</div>'
+    document.getElementById("st1").innerHTML='<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">=25</div>'
+}
+document.getElementById("my-button7").onclick=function(){
+    document.getElementById("st").innerHTML='<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">20/5</div>'
+    document.getElementById("st1").innerHTML='<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">=4</div>'
+}
+document.getElementById("my-button8").onclick=function(){
+    document.getElementById("st").innerHTML='<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">36 / 6 * 3 + 2 ** 4 - (3 + 5)</div>'
+    document.getElementById("st1").innerHTML='<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">=26</div>'
+}
+document.getElementById("demo").onclick=function(){
+    document.getElementById("st").innerHTML=" "
+}
+document.getElementById("demo1").onclick=function(){
+    document.getElementById("st1").innerHTML=" "
+}
